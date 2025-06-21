76e38ea7be928578ee89dae5764791a3bbc59f54
Jordi Vilagut Treserra <vilagutjordi@gmail.com>
2025-06-22 00:17:39 +0200

    Change s3 config

--- modules/s3/main.tf
+++ modules/s3/main.tf
@@ -27,17 +27,19 @@ resource "aws_s3_bucket_public_access_block" "public_access" {
   restrict_public_buckets = true
 }
 
+resource "aws_cloudfront_origin_access_control" "oac" {
+  name                              = "${var.project_name}-oac"
+  description                       = "OAC for ${var.project_name}"
+  origin_access_control_origin_type = "s3"
+  signing_behavior                  = "always"
+  signing_protocol                  = "sigv4"
+}
+
 resource "aws_cloudfront_distribution" "cdn" {
   origin {
-    domain_name = aws_s3_bucket_website_configuration.website.website_endpoint
-    origin_id   = "s3-${aws_s3_bucket.website.bucket}"
-
-    custom_origin_config {
-      http_port              = 80
-      https_port             = 443
-      origin_protocol_policy = "http-only"
-      origin_ssl_protocols   = ["TLSv1.2"]
-    }
+    domain_name              = aws_s3_bucket.website.bucket_regional_domain_name
+    origin_id                = "s3-${aws_s3_bucket.website.bucket}"
+    origin_access_control_id = aws_cloudfront_origin_access_control.oac.id
   }
 
   enabled             = true
@@ -48,6 +50,10 @@ resource "aws_cloudfront_distribution" "cdn" {
     allowed_methods  = ["GET", "HEAD"]
     cached_methods   = ["GET", "HEAD"]
     target_origin_id = "s3-${aws_s3_bucket.website.bucket}"
+    viewer_protocol_policy = "redirect-to-https"
+    min_ttl     = 0
+    default_ttl = 3600
+    max_ttl     = 86400
 
     forwarded_values {
       query_string = false
@@ -55,11 +61,6 @@ resource "aws_cloudfront_distribution" "cdn" {
         forward = "none"
       }
     }
-
-    viewer_protocol_policy = "redirect-to-https"
-    min_ttl                = 0
-    default_ttl            = 3600
-    max_ttl                = 86400
   }
 
   viewer_certificate {
@@ -75,25 +76,6 @@ resource "aws_cloudfront_distribution" "cdn" {
   tags = {
     Name = "${var.project_name}-cdn"
   }
-}
 
-resource "aws_s3_bucket_policy" "private_access" {
-  bucket = aws_s3_bucket.website.id
-
-  policy = jsonencode({
-    Version = "2012-10-17",
-    Statement = [
-      {
-        Sid: "PublicReadGetObject",
-        Effect: "Allow",
-        Principal: "*",
-        Action: "s3:GetObject",
-        Resource: "${aws_s3_bucket.website.arn}/*"
-      }
-    ]
-  })
-
-  depends_on = [
-    aws_cloudfront_distribution.cdn
-  ]
+  depends_on = [aws_cloudfront_origin_access_control.oac]
 }
