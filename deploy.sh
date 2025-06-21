d818a21d9d50cd57a3322434e854a4fd73b27e2c
Jordi Vilagut Treserra <vilagutjordi@gmail.com>
2025-06-21 22:58:12 +0200

    Use s3 bucket to keep infra state

--- deploy.sh
+++ deploy.sh
@@ -3,11 +3,17 @@
 export "$(grep -v '^#' .env | xargs)"
 set -e
 
-terraform destroy -auto-approve \
-  -var="region=$REGION" \
-  -var="project_id=$PROJECT_ID" \
-  -var="project_name=$PROJECT_NAME" \
-  -var="ami_id=$AMI_ID" \
-  -var="db_name=$DB_NAME" \
-  -var="db_user=$DB_USERNAME" \
-  -var="db_password=$PLANTER_DB_PASSWORD"
+terraform init \
+  -backend-config="bucket=codefarm-terraform-states" \
+  -backend-config="key=users/${GITHUB_USER}/${PROJECT_NAME}/terraform.tfstate" \
+  -backend-config="region=${AWS_REGION}" \
+  -backend-config="encrypt=true"
+
+terraform apply -auto-approve \
+  -var="region=${AWS_REGION}" \
+  -var="project_id=${PROJECT_ID}" \
+  -var="project_name=${PROJECT_NAME}" \
+  -var="ami_id=${AMI_ID}" \
+  -var="db_name=${DB_NAME}" \
+  -var="db_user=${DB_USERNAME}" \
+  -var="db_password=${PLANTER_DB_PASSWORD}"
--- destroy.sh
+++ destroy.sh
@@ -1,13 +1,22 @@
 #!/bin/bash
 
+# Load environment variables
 export "$(grep -v '^#' .env | xargs)"
 set -e
 
+# Init backend with same key (important for destroy)
+terraform init \
+  -backend-config="bucket=codefarm-terraform-states" \
+  -backend-config="key=users/${GITHUB_USER}/${PROJECT_NAME}/terraform.tfstate" \
+  -backend-config="region=${AWS_REGION}" \
+  -backend-config="encrypt=true"
+
+# Destroy infrastructure
 terraform destroy -auto-approve \
-  -var="region=$REGION" \
-  -var="project_id=$PROJECT_ID" \
-  -var="project_name=$PROJECT_NAME" \
-  -var="ami_id=$AMI_ID" \
-  -var="db_name=$DB_NAME" \
-  -var="db_user=$DB_USERNAME" \
-  -var="db_password=$PLANTER_DB_PASSWORD"
+  -var="region=${AWS_REGION}" \
+  -var="project_id=${PROJECT_ID}" \
+  -var="project_name=${PROJECT_NAME}" \
+  -var="ami_id=${AMI_ID}" \
+  -var="db_name=${DB_NAME}" \
+  -var="db_user=${DB_USERNAME}" \
+  -var="db_password=${PLANTER_DB_PASSWORD}"
