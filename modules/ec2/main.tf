104bedeba4182f20d1819f451d9d0c2c2a3bb65b
Jordi Vilagut Treserra <vilagutjordi@gmail.com>
2025-06-22 13:47:40 +0200

    Flexibilise access rules to allow github to deploy inside ec2

--- modules/ec2/main.tf
+++ modules/ec2/main.tf
@@ -24,6 +24,14 @@ resource "aws_security_group" "ec2_sg" {
   }
 
   ingress {
+    description = "temp-github-all-access"
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  ingress {
     description = "all-http-traffic"
     from_port   = 80
     to_port     = 80
