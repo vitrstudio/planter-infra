223f8f7bc310c3c28947a19e0ed0c4ccbbbba640
Jordi Vilagut Treserra <vilagutjordi@gmail.com>
2025-06-22 15:52:19 +0200

    Add website policy

--- modules/s3/main.tf
+++ modules/s3/main.tf
@@ -79,3 +79,28 @@ resource "aws_cloudfront_distribution" "cdn" {
 
   depends_on = [aws_cloudfront_origin_access_control.oac]
 }
+
+resource "aws_s3_bucket_policy" "website_policy" {
+  bucket = aws_s3_bucket.website.id
+
+  policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [
+      {
+        Sid       = "AllowCloudFrontServicePrincipalReadOnly"
+        Effect    = "Allow"
+        Principal = {
+          Service = "cloudfront.amazonaws.com"
+        }
+        Action = "s3:GetObject"
+        Resource = "${aws_s3_bucket.website.arn}/*"
+        Condition = {
+          StringEquals = {
+            "AWS:SourceArn" = aws_cloudfront_distribution.cdn.arn
+          }
+        }
+      }
+    ]
+  })
+}
+
