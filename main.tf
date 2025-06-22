a2de97bbaf06363c9c67701951d888471cc38ffd
Jordi Vilagut Treserra <vilagutjordi@gmail.com>
2025-06-22 08:35:04 +0200

    Avoid preinstalling docker on EC2 and add all access for EC2 while we dont find a solution for github to ssh into the server

--- main.tf
+++ main.tf
@@ -26,7 +26,6 @@ module "ec2" {
   vpc_id       = module.vpc.vpc_id
   subnet_id    = module.vpc.public_subnet_id
   ami_id       = var.ami_id
-  user_data = file("${path.module}/scripts/docker-user-data.sh")
 }
 
 module "s3" {
