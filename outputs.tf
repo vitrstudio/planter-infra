0fecd1901999fd0e6c7d4f72803b5f76bdcfdc63
Jordi Vilagut Treserra <vilagutjordi@gmail.com>
2025-07-12 18:00:51 +0200

    Store ec2 instance id in secrets

--- outputs.tf
+++ outputs.tf
@@ -3,6 +3,11 @@ output "ec2_public_ip" {
   value       = module.ec2.public_ip
 }
 
+output "ec2_instance_id" {
+  description = "Instance ID of the EC2 instance"
+  value       = module.ec2.instance_id
+}
+
 output "rds_endpoint" {
   description = "Endpoint of the RDS instance"
   value       = module.rds.rds_endpoint
