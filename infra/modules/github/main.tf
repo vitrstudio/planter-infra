60d02f6a2665048dcbf1c31539df226aacd597f9
Jordi Vilagut Treserra <vilagutjordi@gmail.com>
2025-07-27 21:39:25 +0200

    Fix github actions permissions

--- infra/modules/github/main.tf
+++ infra/modules/github/main.tf
@@ -1,64 +1,12 @@
 variable "project_name" {}
-variable "repository_name" {}
-variable "aws_account_id" {}
+variable "github_repo" {
+  description = "Github organisation/repository"
+}
 
 resource "aws_iam_openid_connect_provider" "github" {
-  url = "https://token.actions.githubusercontent.com"
-
-  client_id_list = [
-    "sts.amazonaws.com"
-  ]
-
-  thumbprint_list = [
-    "6938fd4d98bab03faadb97b34396831e3780aea1"
-  ]
-}
-
-resource "aws_iam_role" "github_oidc" {
-  name = "${var.project_name}-github-oidc-role"
-
-  assume_role_policy = jsonencode({
-    Version = "2012-10-17",
-    Statement = [
-      {
-        Effect = "Allow",
-        Principal = {
-          Federated = aws_iam_openid_connect_provider.github.arn
-        },
-        Action = "sts:AssumeRoleWithWebIdentity",
-        Condition = {
-          StringEquals = {
-            "token.actions.githubusercontent.com:aud" = "sts.amazonaws.com",
-            "token.actions.githubusercontent.com:sub" = "repo:${var.repository_name}:ref:refs/heads/main"
-          }
-        }
-      }
-    ]
-  })
-}
-
-resource "aws_iam_policy" "ssm_github_policy" {
-  name = "${var.project_name}-ssm-github-policy"
-
-  policy = jsonencode({
-    Version = "2012-10-17",
-    Statement = [
-      {
-        Effect = "Allow",
-        Action = [
-          "ssm:SendCommand",
-          "ssm:GetCommandInvocation",
-          "ssm:ListCommandInvocations"
-        ],
-        Resource = "*"
-      }
-    ]
-  })
-}
-
-resource "aws_iam_role_policy_attachment" "ssm_attach" {
-  role       = aws_iam_role.github_oidc.name
-  policy_arn = aws_iam_policy.ssm_github_policy.arn
+  url            = "https://token.actions.githubusercontent.com"
+  client_id_list = ["sts.amazonaws.com"]
+  thumbprint_list= ["6938fd4d98bab03faadb97b34396831e3780aea1"]
 }
 
 resource "aws_iam_role" "github_actions" {
@@ -66,41 +14,47 @@ resource "aws_iam_role" "github_actions" {
 
   assume_role_policy = jsonencode({
     Version = "2012-10-17",
-    Statement = [
-      {
-        Effect = "Allow",
-        Principal = {
-          Federated = aws_iam_openid_connect_provider.github.arn
-        },
-        Action = "sts:AssumeRoleWithWebIdentity",
-        Condition = {
-          StringLike = {
-            "token.actions.githubusercontent.com:sub" = "repo:${var.repository_name}:*"
-          }
+    Statement = [{
+      Effect    = "Allow",
+      Principal = {
+        Federated = aws_iam_openid_connect_provider.github.arn
+      },
+      Action    = "sts:AssumeRoleWithWebIdentity",
+      Condition = {
+        StringEquals = {
+          "token.actions.githubusercontent.com:aud" = "sts.amazonaws.com",
+          "token.actions.githubusercontent.com:sub" = "repo:${var.github_repo}:ref:refs/heads/main"
         }
       }
-    ]
+    }]
   })
 }
 
-resource "aws_iam_role_policy" "github_permissions" {
-  name = "${var.project_name}-github-policy"
-  role = aws_iam_role.github_actions.id
+resource "aws_iam_policy" "github_actions_perms" {
+  name = "${var.project_name}-github-perms"
 
   policy = jsonencode({
     Version = "2012-10-17",
-    Statement = [
-      {
-        Effect   = "Allow",
-        Action   = [
-          "ssm:SendCommand",
-          "ec2:DescribeInstances",
-          "s3:PutObject",
-          "s3:GetObject",
-          "s3:ListBucket"
-        ],
-        Resource = "*"
-      }
-    ]
+    Statement = [{
+      Effect = "Allow",
+      Action = [
+        "ssm:GetParameter",
+        "ssm:GetParameters",
+        "ssm:SendCommand",
+        "ssm:GetCommandInvocation",
+        "ssm:ListCommandInvocations",
+        "ec2:DescribeInstances",
+        "s3:PutObject",
+        "s3:GetObject",
+        "s3:ListBucket"
+      ],
+
+      Resource = "*"
+    }]
   })
+}
+
+resource "aws_iam_role_policy_attachment" "attach_perms" {
+  role       = aws_iam_role.github_actions.name
+  policy_arn = aws_iam_policy.github_actions_perms.arn
 }
