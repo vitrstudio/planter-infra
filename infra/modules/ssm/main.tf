04b8245f6b7fa8beb8a493a942763c560882d24d
Jordi Vilagut Treserra <vilagutjordi@gmail.com>
2025-08-03 17:20:50 +0200

    Overwrite ssm parameters

--- infra/modules/ssm/main.tf
+++ infra/modules/ssm/main.tf
@@ -1,8 +1,8 @@
-# SSM parameters
 resource "aws_ssm_parameter" "static_website_cloudfront_id" {
-  name  = "/${var.project_name}/static_website_cloudfront_id"
-  type  = "String"
-  value = var.static_website_cloudfront_id
+  name      = "/${var.project_name}/static_website_cloudfront_id"
+  type      = "String"
+  value     = var.static_website_cloudfront_id
+  overwrite = true
 
   tags = {
     Name = "${var.project_name}-static-website-cloudfront-id"
@@ -10,9 +10,10 @@ resource "aws_ssm_parameter" "static_website_cloudfront_id" {
 }
 
 resource "aws_ssm_parameter" "ec2_instance_id" {
-  name  = "/${var.project_name}/ec2_instance_id"
-  type  = "String"
-  value = var.ec2_instance_id
+  name      = "/${var.project_name}/ec2_instance_id"
+  type      = "String"
+  value     = var.ec2_instance_id
+  overwrite = true
 
   tags = {
     Name = "${var.project_name}-ec2-instance-id"
@@ -20,9 +21,10 @@ resource "aws_ssm_parameter" "ec2_instance_id" {
 }
 
 resource "aws_ssm_parameter" "ec2_public_ip" {
-  name  = "/${var.project_name}/ec2_public_ip"
-  type  = "String"
-  value = var.ec2_public_ip
+  name      = "/${var.project_name}/ec2_public_ip"
+  type      = "String"
+  value     = var.ec2_public_ip
+  overwrite = true
 
   tags = {
     Name = "${var.project_name}-ec2-public-ip"
@@ -30,9 +32,10 @@ resource "aws_ssm_parameter" "ec2_public_ip" {
 }
 
 resource "aws_ssm_parameter" "rds_endpoint" {
-  name  = "/${var.project_name}/rds_endpoint"
-  type  = "String"
-  value = var.rds_endpoint
+  name      = "/${var.project_name}/rds_endpoint"
+  type      = "String"
+  value     = var.rds_endpoint
+  overwrite = true
 
   tags = {
     Name = "${var.project_name}-rds-endpoint"
@@ -40,9 +43,10 @@ resource "aws_ssm_parameter" "rds_endpoint" {
 }
 
 resource "aws_ssm_parameter" "deployment_bucket" {
-  name  = "/${var.project_name}/deployment_bucket"
-  type  = "String"
-  value = var.deployment_bucket
+  name      = "/${var.project_name}/deployment_bucket"
+  type      = "String"
+  value     = var.deployment_bucket
+  overwrite = true
 
   tags = {
     Name = "${var.project_name}-deployment-bucket"
@@ -50,9 +54,10 @@ resource "aws_ssm_parameter" "deployment_bucket" {
 }
 
 resource "aws_ssm_parameter" "github_role_arn" {
-  name  = "/${var.project_name}/github_role_arn"
-  type  = "String"
-  value = var.github_role_arn
+  name      = "/${var.project_name}/github_role_arn"
+  type      = "String"
+  value     = var.github_role_arn
+  overwrite = true
 
   tags = {
     Name = "${var.project_name}-github-role-arn"
